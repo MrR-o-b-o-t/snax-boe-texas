cf991c96b9079600b7891527403bc0d02f40019d
Web-Dev-Jr <samkautz@yahoo.com>
2021-07-28 17:05:20 -0500

    update mobile home display for loan cards. Update button links in header of home page

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home"
-export { default as LoanProducts } from "./LoanProducts"
-export { default as Contact } from "./Contact"
-export { default as Resources } from "./Resources"
-export { default as Team } from "./Team"
