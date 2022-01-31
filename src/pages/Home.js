59232cb2bb75b52451b5663116a892855ff35c26
Web-Dev-Jr <samkautz@yahoo.com>
2022-01-31 09:55:51 -0600

    update logo and remove Apply Now button from Header.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,32 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from 'react'
 import {
   MDBBtn,
   MDBContainer,
   MDBRow,
   MDBCol,
   MDBAnimation,
-} from "mdb-react-ui-kit";
+} from 'mdb-react-ui-kit'
 
-import house from "../images/Bank-of-England-House-Illustration.png";
-import customer1 from "../images/Bank-of-England-Customer.jpg";
-import signing from "../images/boe-office-front.jpg";
-import Reviews from "../components/Reviews";
-import LoanCards from "../components/LoanCards";
-import LightBanner from "../components/LightBanner";
-import VideoTest from "../components/VideoTest";
-import ClientTest from "../components/ClientTest";
+import house from '../images/Bank-of-England-House-Illustration.png'
+import customer1 from '../images/Bank-of-England-Customer.jpg'
+import signing from '../images/boe-office-front.jpg'
+import Reviews from '../components/Reviews'
+import LoanCards from '../components/LoanCards'
+import LightBanner from '../components/LightBanner'
+import VideoTest from '../components/VideoTest'
+import ClientTest from '../components/ClientTest'
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export default function Home() {
-  gsap.registerPlugin(ScrollTrigger);
-  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger)
+  const ref = useRef(null)
 
   useEffect(() => {
-    const element = ref.current;
+    const element = ref.current
     gsap.fromTo(
-      element.querySelector("#move"),
+      element.querySelector('#move'),
       {
         opacity: -9,
         y: 240,
@@ -35,19 +35,19 @@ export default function Home() {
         opacity: 4,
         y: -50,
         scrollTrigger: {
-          trigger: element.querySelector("#here"),
-          start: "top top",
-          end: "bottom center",
+          trigger: element.querySelector('#here'),
+          start: 'top top',
+          end: 'bottom center',
           scrub: true,
         },
       }
-    );
-  }, []);
+    )
+  }, [])
 
   useEffect(() => {
-    const element = ref.current;
+    const element = ref.current
     gsap.fromTo(
-      element.querySelector("#ma__banner__house"),
+      element.querySelector('#ma__banner__house'),
       {
         opacity: 1,
         x: 450,
@@ -56,126 +56,126 @@ export default function Home() {
         opacity: 1,
         x: 0,
         scrollTrigger: {
-          trigger: element.querySelector("#here"),
-          start: "top center",
-          end: "bottom center",
+          trigger: element.querySelector('#here'),
+          start: 'top center',
+          end: 'bottom center',
           scrub: true,
         },
       }
-    );
-  }, []);
+    )
+  }, [])
 
   return (
-    <div style={{ overflowX: "hidden" }}>
-      <MDBContainer fluid style={{ maxWidth: "2000px", padding: "0" }}>
-        <MDBContainer className="" style={{ marginTop: "100px" }}>
-          <MDBRow className="g-2">
-            <MDBCol lg="6" className="user-select-none mb-3">
-              <div style={{ paddingLeft: "15px" }}>
+    <div style={{ overflowX: 'hidden' }}>
+      <MDBContainer fluid style={{ maxWidth: '2000px', padding: '0' }}>
+        <MDBContainer className='' style={{ marginTop: '100px' }}>
+          <MDBRow className='g-2'>
+            <MDBCol lg='6' className='user-select-none mb-3'>
+              <div style={{ paddingLeft: '15px' }}>
                 <h1
-                  class="text-md-start"
-                  style={{ fontWeight: "bold", color: "black" }}
+                  class='text-md-start'
+                  style={{ fontWeight: 'bold', color: 'black' }}
                 >
                   <div
                     style={{
-                      color: "#215732",
-                      width: "50px",
-                      height: "7px",
-                      background: "#215732",
-                      marginBottom: "15px",
+                      color: '#215732',
+                      width: '50px',
+                      height: '7px',
+                      background: '#215732',
+                      marginBottom: '15px',
                     }}
                   />
                   Bank of England Mortgage is the white glove home loan
                   experience designed for you
                 </h1>
-                <h5 class="text-left">
+                <h5 class='text-left'>
                   The home buying experience shouldn't be a hassle. With BOE
                   Texas it won't be. Start an application to see what's
                   possible.
                 </h5>
               </div>
-              <div className="text-center">
+              <div className='text-center'>
                 <MDBBtn
-                  id="m__btn"
-                  tag="a"
-                  className="m-2 p-3"
+                  id='m__btn'
+                  tag='a'
+                  className='m-2 p-3'
                   style={{
-                    fontWeight: "600",
-                    width: "300px",
-                    fontSize: "16px",
+                    fontWeight: '600',
+                    width: '300px',
+                    fontSize: '16px',
                   }}
-                  target="_blank"
-                  href="https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com"
+                  target='_blank'
+                  href='https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com'
                 >
                   Apply For Home Purchase
                 </MDBBtn>
               </div>
-              <div className="text-center">
+              <div className='text-center'>
                 <MDBBtn
-                  id="m__btn"
-                  tag="a"
-                  className="m-2 p-3"
+                  id='m__btn'
+                  tag='a'
+                  className='m-2 p-3'
                   style={{
-                    fontWeight: "600",
-                    width: "300px",
-                    fontSize: "16px",
+                    fontWeight: '600',
+                    width: '300px',
+                    fontSize: '16px',
                   }}
-                  target="_blank"
-                  href="https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com"
+                  target='_blank'
+                  href='https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com'
                 >
                   Apply For Refinance
                 </MDBBtn>
               </div>
             </MDBCol>
             <MDBCol
-              lg="6"
+              lg='6'
               style={{
                 backgroundImage: `url(${customer1})`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "top center",
-                backgroundSize: "cover",
-                minHeight: "40vh",
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'top center',
+                backgroundSize: 'cover',
+                minHeight: '40vh',
               }}
             ></MDBCol>
           </MDBRow>
         </MDBContainer>
         <LightBanner />
-        <MDBContainer className="mt-3" style={{ maxWidth: "2500px" }}>
-          <MDBRow className="g-2">
-            <MDBCol lg="6" className="g-0" id="lb__parallax__img">
-              <img src={signing} class="img-fluid mb-4"></img>
+        <MDBContainer className='mt-3' style={{ maxWidth: '2500px' }}>
+          <MDBRow className='g-2'>
+            <MDBCol lg='6' className='g-0' id='lb__parallax__img'>
+              <img src={signing} class='img-fluid mb-4'></img>
             </MDBCol>
-            <MDBCol lg="6" className="">
-              <div style={{ marginLeft: "15px" }}>
+            <MDBCol lg='6' className=''>
+              <div style={{ marginLeft: '15px' }}>
                 <h1
-                  class="text-md-start"
-                  style={{ fontWeight: "bold", color: "black" }}
+                  class='text-md-start'
+                  style={{ fontWeight: 'bold', color: 'black' }}
                 >
                   <div
                     style={{
-                      color: "#215732",
-                      width: "50px",
-                      height: "7px",
-                      background: "#215732",
-                      marginBottom: "15px",
+                      color: '#215732',
+                      width: '50px',
+                      height: '7px',
+                      background: '#215732',
+                      marginBottom: '15px',
                     }}
                   />
                   Connect with a lender in your community.
                 </h1>
-                <h5 class="text-left">
+                <h5 class='text-left'>
                   A mortgage broker can be your trusted guide close to home.
                   They can really get to know you and tailer loan options to
                   meet your needs.
                 </h5>
               </div>
-              <div className="text-center mt-4">
+              <div className='text-center mt-4'>
                 <MDBBtn
-                  id="s__btn"
-                  tag="a"
-                  rippleColor="success"
-                  className="m-2"
-                  href="https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com"
-                  target="_blank"
+                  id='s__btn'
+                  tag='a'
+                  rippleColor='success'
+                  className='m-2'
+                  href='https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com'
+                  target='_blank'
                 >
                   Connect With Your Lender
                 </MDBBtn>
@@ -186,16 +186,16 @@ export default function Home() {
           <LoanCards />
         </MDBContainer>
         <MDBContainer
-          style={{ backgroundColor: "#4e4e4e", maxWidth: "2500px" }}
-          className="mt-5 mb-5"
+          style={{ backgroundColor: '#4e4e4e', maxWidth: '2500px' }}
+          className='mt-5 mb-5'
           ref={ref}
         >
           <MDBRow
-            className="d-flex justify-content-center align-items-center p-5"
-            style={{ color: "white" }}
+            className='d-flex justify-content-center align-items-center p-5'
+            style={{ color: 'white' }}
           >
-            <MDBCol sm="4">
-              <h1 style={{ fontWeight: "bold" }} id="move">
+            <MDBCol sm='4'>
+              <h1 style={{ fontWeight: 'bold' }} id='move'>
                 Your Mortgage Approval Starts Here
               </h1>
               <p>
@@ -203,39 +203,22 @@ export default function Home() {
                 accurate numbers fast.
               </p>
               <MDBBtn
-                id="s__btn"
-                tag="a"
-                rippleColor="success"
-                href="https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com"
-                target="_blank"
+                id='s__btn'
+                tag='a'
+                rippleColor='success'
+                href='https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com'
+                target='_blank'
               >
                 Start My Approval
               </MDBBtn>
             </MDBCol>
-            <MDBCol sm="4" className="text-center">
-              <img src={house} class="img-fluid" id="ma__banner__house" />
+            <MDBCol sm='4' className='text-center'>
+              <img src={house} class='img-fluid' id='ma__banner__house' />
             </MDBCol>
           </MDBRow>
         </MDBContainer>
         <Reviews />
       </MDBContainer>
-      {/* <MDBBtn
-        id="m__btn"
-        tag="a"
-        className="m-2 p-3"
-        style={{
-          fontWeight: "600",
-          fontSize: "16px",
-          position: "fixed",
-          bottom: "25px",
-          right: "10px",
-          zIndex: "2",
-        }}
-        target="_blank"
-        href="https://boeedge.boemortgage.com/borrower/signup/infonow@boemortgage.com"
-      >
-        Apply Now
-      </MDBBtn> */}
     </div>
-  );
+  )
 }
